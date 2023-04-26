1526a31a0cf418d5105218b70b4936cc4f490d4f
Martin Heir <mbheir@gmail.com>
2023-04-26 19:34:53 +0200

    updated guidance

--- controller_node/src/controller.cpp
+++ controller_node/src/controller.cpp
@@ -1,101 +1,127 @@
-#include "controller.h"
-
-USVController::USVController(ros::NodeHandle& nh) {
-  m_speedCourseSub = nh.subscribe("speed_course", 10, &USVController::speedCourseCallback, this);
-  m_poseTwistSub = nh.subscribe("p3d", 10, &USVController::poseTwistCallback, this);
-  m_leftPub = nh.advertise<std_msgs::Float32>("left_thrust_cmd", 10);
-  m_rightPub = nh.advertise<std_msgs::Float32>("right_thrust_cmd", 10);
-
-  m_desiredSpeed = 0.0;
-  m_desiredCourse = 0.0;
-  m_currentSpeed = 0.0;
-  m_currentYaw = 0.0;
-
-  m_thrusterConfigMatrix = Eigen::MatrixXd::Zero(3, 2);
-  m_thrusterConfigMatrix << 50, 50, 0, 0, -0.39 * 50, 0.39 * 50;
-
-  // Initialize integral error accumulators
-  m_speedIntegralError = 0.0;
-  m_courseIntegralError = 0.0;
-
-  // Initialize integral gains
-  m_Ki_speed = 0.1;
-  m_Ki_course = 0.1;
-}
-
-void USVController::speedCourseCallback(const usv_msgs::SpeedCourse& msg) {
-  m_desiredSpeed = msg.speed;
-  m_desiredCourse = msg.course;
-}
-
-void USVController::poseTwistCallback(const nav_msgs::Odometry& msg) {
-  m_currentSpeed = msg.twist.twist.linear.x;
-  m_currentYaw = tf::getYaw(msg.pose.pose.orientation);
-}
-
-void USVController::controlLoop() {
-  double Kp_speed = 5.0;  // Proportional gain for speed
-  double Kp_course = 4.0; // Proportional gain for course
-
-  double dt = 0.1; // Time step for the control loop (1 / loop_rate in the main function)
+#include "los_guidance.h"
 
 
-  double speed_error = m_desiredSpeed - m_currentSpeed;
-  double course_error = m_desiredCourse - m_currentYaw;
-
-  // Wrap course_error to [-pi, pi]
-  while (course_error > M_PI) course_error -= 2 * M_PI;
-  while (course_error < -M_PI) course_error += 2 * M_PI;
-
-  // Print speed and heading error
-  ROS_INFO("Speed error(m/s): %f, Heading error(deg): %f", speed_error, course_error * 180.0 / M_PI);
-
-  // Update integral error accumulators
-  m_speedIntegralError += speed_error * dt;
-  m_courseIntegralError += course_error * dt;
-
-  // Calculate control inputs using proportional and integral terms
-  double u_speed = Kp_speed * speed_error + m_Ki_speed * m_speedIntegralError;
-  double u_course = Kp_course * course_error + m_Ki_course * m_courseIntegralError;
-
-  Eigen::VectorXd control_input(3);
-  control_input << u_speed, 0, u_course;
-
-  static bool initialized = false;
-  static Eigen::MatrixXd pinv(3, 2);
-  if (!initialized) {
-  initialized = true;
-  pinv = m_thrusterConfigMatrix.completeOrthogonalDecomposition().pseudoInverse();
-
-  // Print thruster configuration matrix initialization
-  ROS_INFO("Thruster configuration matrix initialized");
-  }
-
-  Eigen::VectorXd thruster_output = pinv * control_input;
-
-  std_msgs::Float32 left_thrust_msg;
-  std_msgs::Float32 right_thrust_msg;
-
-  left_thrust_msg.data = thruster_output(0);
-  right_thrust_msg.data = thruster_output(1);
-
-  m_leftPub.publish(left_thrust_msg);
-  m_rightPub.publish(right_thrust_msg);
-  }
-
-int main(int argc, char** argv) {
-  ros::init(argc, argv, "usv_controller");
+LOSGuidance::LOSGuidance()
+{
   ros::NodeHandle nh;
 
-  USVController controller(nh);
+  m_pathSub = nh.subscribe("path", 1000, &LOSGuidance::pathCallback, this);
+  m_poseSub = nh.subscribe("p3d", 1000, &LOSGuidance::poseCallback, this);
 
-  ros::Rate loop_rate(10); // Control loop rate (Hz)
+  m_speedCoursePub = nh.advertise<usv_msgs::SpeedCourse>("speed_course", 10);
+}
 
-  while (ros::ok()) {
-    controller.controlLoop();
-    ros::spinOnce();
-    loop_rate.sleep();
+void LOSGuidance::pathCallback(const nav_msgs::Path& path)
+{
+  m_path = path;
+}
+
+void LOSGuidance::poseCallback(const nav_msgs::Odometry& pose)
+{
+  double x = pose.pose.pose.position.x;
+  double y = pose.pose.pose.position.y;
+  double psi = tf2::getYaw(pose.pose.pose.orientation);
+
+  u = pose.twist.twist.linear.x;
+  r = pose.twist.twist.angular.z;
+
+  followPath(x, y, psi);
+}
+
+void LOSGuidance::followPath(double x, double y, double psi)
+{
+  // Implement the LOS guidance logic similar to the provided code
+  // TODO: Customize the logic for the specific /path and /p3d messages in this implementation
+
+  // Finished?
+  if (m_path.poses.size() <= 1)
+  {
+    usv_msgs::SpeedCourse msg;
+    msg.speed = 0.0;
+    msg.course = psi;
+    m_speedCoursePub.publish(msg);
+    return;
   }
+
+  // Identify closest point on path
+  std::vector<geometry_msgs::PoseStamped>::iterator closest;
+  double minDist = std::numeric_limits<double>::max();
+  for (auto it = m_path.poses.begin(); it != m_path.poses.end(); it++)
+  {
+    double dist = std::sqrt(std::pow(x - it->pose.position.x, 2) +
+                            std::pow(y - it->pose.position.y, 2));
+    if (dist < minDist)
+    {
+      minDist = dist;
+      closest = it;
+    }
+  }
+
+  // Store closest
+  geometry_msgs::PoseStamped pose_d = *closest;
+
+  // Erase previous elements
+  m_path.poses.erase(m_path.poses.begin(), closest);
+
+  // Path tangential angle
+  double gamma_p = tf2::getYaw(pose_d.pose.orientation);
+
+  // Cross-track error
+  double y_e = -(x - pose_d.pose.position.x) * std::sin(gamma_p) +
+               (y - pose_d.pose.position.y) * std::cos(gamma_p);
+
+  // Time-varying lookahead distance
+  double delta_y_e =
+      (delta_max - delta_min) * std::exp(-delta_k * std::pow(y_e, 2)) +
+      delta_min;
+  // if turning => small lookahead distance
+  bool isTurning = false;
+  if ((closest + 1) != m_path.poses.end())
+  {
+    double nextAngle = tf2::getYaw((*(closest + 1)).pose.orientation);
+    if (std::fabs(gamma_p - nextAngle) > std::numeric_limits<double>::epsilon())
+    {
+      delta_y_e = delta_min;
+      isTurning = true;
+    }
+  }
+
+  // velocity-path relative angle
+  double chi_r = std::atan(-y_e / delta_y_e);
+
+  // desired course angle
+  double chi_d = gamma_p + chi_r;
+
+  // calculate error in heading
+  double chi_err = chi_d - psi;
+  while (chi_err > M_PI)
+  {
+    chi_err -= 2 * M_PI;
+  }
+  while (chi_err < -M_PI)
+  {
+    chi_err += 2 * M_PI;
+  }
+
+  // calculate desired speed
+  double u = m_maxSpeed * (1 - std::abs(y_e) / 5 - std::abs(chi_err) / M_PI_2);
+  u = std::max(u, m_minSpeed);
+  if (isTurning)
+    u = m_maxSpeedTurn;
+
+  // Publish speed and course to controller
+  usv_msgs::SpeedCourse msg;
+  msg.speed = u;
+  msg.course = chi_d;
+  m_speedCoursePub.publish(msg);
+}
+
+int main(int argc, char* argv[])
+{
+  ros::init(argc, argv, "losguidance_node");
+  LOSGuidance losGuidance;
+
+  ros::spin();
 
   return 0;
 }
